91d253428d132ce6881779eeb551cec98c90e6b9
Arthur VILLARD <villardarthur@gmail.com>
2022-10-27 15:50:12 +0200

    Init TP3

---
+++ TP/TP3/main.cpp
@@ -0,0 +1,19 @@
+//Classe Grid abstraite
+//classe MorpionGrid hérite de Grid
+//classe Puissance4Grid hérite de Grid
+//classe Game
+
+//Mode multi ou solo vs ia
+//menu principal
+#include <iostream>
+
+int main()
+{
+	bool run = true;
+	while (run)
+	{
+		std::cout << "Bonjour\n";
+		run = false;
+	}
+	return 0;
+}
