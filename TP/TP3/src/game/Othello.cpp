8fb1c7e6cbeb3dbc1339d76a852919d8110a3816
Alexandre <alxandre.rbrt@gmail.com>
2022-12-20 13:32:49 +0100

    Othello : problème vérification au bords

--- TP/TP3/src/game/Othello.cpp
+++ TP/TP3/src/game/Othello.cpp
@@ -110,21 +110,82 @@ void Othello::replace_symbolBetween(int& symbol, int& column, int& line)
 	// *** FONCTIONNEMENT METHODE *** //
 	//1. verifie dans les 8 directions si il y a un symbole du même type, note la position dans un tableau last_symbol
 	//2. remplace les symboles entre la case jouée et la position de last_symbol pour chaque direction
-	
-	std::cout << "Entering replace_symbolBetween" << std::endl;
 
 	int directions[8][2] = { {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0}, {-1,1} };
 	int last_symbol[8][2] = { {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0}, {0,0} };
 	
-	//1.
-	//pour les 8 directions
+	std::cout << "Position of the symbol played  : x = " << column << " y = " << line << " | Symbole : " << grid->get_element(line, column) << std::endl;
+	
+	//1. verifie dans les 8 directions si il y a un symbole du même type, note la position dans un tableau last_symbol
 	for (int i = 0; i < 8; i++)
 	{
-		//check_direction(i);
+		int x = column;
+		int y = line;
+		bool other_symbol_found = false;
+		bool same_symbol_found = false;
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//DOING éviter de sortir du tableau lorsque on ignore la position de la case jouée (faire un if ?)
+
+
+
+		//ignore la position de la case jouée en appliquant la direction
+		try
+		{
+			std::cout << "try " << i << " ignoring actual case" << std::endl; // crash ici
+			x += directions[i][0];
+			y += directions[i][1];
+		}
+		catch (const std::exception&)
+		{
+			continue;
+		}
+
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		// tant que la case n'est pas vide et que le symbole n'est pas le même que celui joué ou lui-même
+		while (grid->get_element(y, x) != 0 && grid->get_element(y, x) != symbol)
+		{	
+			std::cout << "Other symbol found at x = " << x << " y = " << y << std::endl;
+			try
+			{
+				std::cout << "try " << i << " direction" << std::endl;
+				x += directions[i][0];
+				y += directions[i][1];
+			}
+			catch (const std::exception&) 
+			{
+				other_symbol_found = true;
+				break;
+			}	
+			other_symbol_found = true;
+		}
+
+		//si le symbole est le même que celui joué et qu'un autre symbole a été trouvé avant
+		if (grid->get_element(y, x) == symbol && other_symbol_found == true)
+		{
+			std::cout << "Same symbol found at x = " << x << " y = " << y << std::endl;
+			last_symbol[i][0] = x;
+			last_symbol[i][1] = y;
+			same_symbol_found = true;
+		}
+		
+		//2. remplace les symboles entre la case jouée et la position de last_symbol s'ils existent
+		if (other_symbol_found && same_symbol_found)
+		{
+			std::cout << "Replace symbols between x = " << column << " y = " << line << " and x = " << last_symbol[i][0] << " y = " << last_symbol[i][1] << std::endl;
+			int x = column;
+			int y = line;
+			while (x != last_symbol[i][0] || y != last_symbol[i][1])
+			{
+				x += directions[i][0];
+				y += directions[i][1];
+				grid->set_element(y, x, symbol);
+			}
+		}
 	}
 }
-	
-
 
 //vérifie si la case est vide
 bool Othello::empty_case(int& column, int& line)
